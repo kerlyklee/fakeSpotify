d4cd9be6e08a361c7e1190c2180ce563874566b8
kerlyklee <kerlyklee@gmail.com>
2019-12-30 23:35:04 +0200

    album cover, volume bar, starting with navigation Bar

--- index.php
+++ index.php
@@ -13,52 +13,90 @@ else {
     <link rel="stylesheet" type="text/css" href="assets/CSS/style.css">
     <title>Spotify rip off</title>
 </head>
-    <div id="nowPlayingContainer">
-
-    <div id="nowPlayingBar">
-        <div id="nowPlayingLeft"></div>
-        <div id="nowPlayingCenter">
-            <div class="content playerControls">
-                <div class="buttons">
-                    <button class="controlButton shuffle" title="Shuffle button">
-                        <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                    </button>
-
-                    <button class="controlButton previous" title="Previous button">
-                        <img src="assets/images/icons/previous.png" alt="Previous">
-                    </button>
-
-                    <button class="controlButton play" title="Play button">
-                        <img src="assets/images/icons/play.png" alt="Play">
-                    </button>
-
-                    <button class="controlButton pause" title="Pause button" style="display: none;">
-                        <img src="assets/images/icons/pause.png" alt="Pause">
-                    </button>
-
-                    <button class="controlButton next" title="Next button">
-                        <img src="assets/images/icons/next.png" alt="Next">
-                    </button>
-
-                    <button class="controlButton repeat" title="Repeat button">
-                        <img src="assets/images/icons/repeat.png" alt="Repeat">
-                    </button>
-                        
-                </div> 
-                <div class="playBar">
-                    <span class="progressTime curent">0.00</span>
-                    <div class="progressBar">
-                        <div class="progressBarBg">
-                             <div class="progress"></div>
-                        </div>
-                    </div>
-                    <span class="progressTime remaining">0.00</span>
-                    
-                </div>
+<body>
+    <div id="mainContainer">
+        <div id="topContainer">
+            <div id="navBarContainer">
+                
             </div>
         </div>
-        <div id="nowPlayingRight"></div>
-    </div>
+        <div id="nowPlayingContainer">
+
+            <div id="nowPlayingBar">
+                <div id="nowPlayingLeft">
+                    <div class="content">
+                        <span class="albumLink">
+                        <img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumCover" alt="Album Cover"> 
+                        </span>
+                        <div class="trackInfo">
+                            <span class="trackName"> 
+                                <span>Happy birthDay</span>
+                            </span>
+                            <span class="trackArtist"> 
+                                <span>Kerly Klee</span>
+                            </span>
+
+                        </div>
+                    </div>
+                </div>
+                <div id="nowPlayingCenter">
+                    <div class="content playerControls">
+                        <div class="buttons">
+                            <button class="controlButton shuffle" title="Shuffle button">
+                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                            </button>
+
+                            <button class="controlButton previous" title="Previous button">
+                                <img src="assets/images/icons/previous.png" alt="Previous">
+                            </button>
+
+                            <button class="controlButton play" title="Play button">
+                                <img src="assets/images/icons/play.png" alt="Play">
+                            </button>
+
+                            <button class="controlButton pause" title="Pause button" style="display: none;">
+                                <img src="assets/images/icons/pause.png" alt="Pause">
+                            </button>
+
+                            <button class="controlButton next" title="Next button">
+                                <img src="assets/images/icons/next.png" alt="Next">
+                            </button>
+
+                            <button class="controlButton repeat" title="Repeat button">
+                                <img src="assets/images/icons/repeat.png" alt="Repeat">
+                            </button>
+                                
+                        </div> 
+                        <div class="playBar">
+                            <span class="progressTime curent">0.00</span>
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="progressTime remaining">0.00</span>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div id="nowPlayingRight">
+                    <div class="volumeBar">
+                        <button class="controlButton volume" title="Volume button">
+                            <img src="assets/images/icons/volume.png" alt="Volume">
+                        </button>
+                        <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress">
+
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>      
+        </div>
 
     </div>
  </body>
